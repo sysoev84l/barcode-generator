776dac1fd969d40082569faa96385a36ff1b9349
sysoev84l <sysoev84l@yandex.ru>
2026-08-21 01:53:36 +0300

    v 0.0.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// let box = document.querySelector('.box');
+//let element = document.createElement('div');
 JsBarcode('#barcode', barCode, {
     format: 'CODE128', // формат штрих-кода
     displayValue: true, // отображать ли текстовое значение под кодом
