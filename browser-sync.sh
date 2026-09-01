2943c8f83855ce957db832da40c80df5ca7f4d1d
sysoev84l <sysoev84l@yandex.ru>
2026-09-02 00:37:18 +0300

    v 0.1.1

--- browser-sync.sh
+++ browser-sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /var/www/barcodegenerator.local
-browser-sync http://barcodegenerator.local --files "*.css,assets/js/*.js,assets/scss/*.scss,assets/css/*.css,*.html,*.*"
+browser-sync http://barcodegenerator.local --files "*.css,assets/php/*.php,assets/js/*.js,assets/scss/*.scss,assets/css/*.css,*.html,*.*"
